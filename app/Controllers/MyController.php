20ab84295b0711e7e31c8f052146be342009bc7e
timothy99 <joydisk@gmail.com>
2022-01-12 23:10:07 +0900

    레이아웃 분리

--- app/Controllers/MyController.php
+++ app/Controllers/MyController.php
@@ -6,6 +6,6 @@ class MyController extends BaseController
 {
     public function __construct()
     {
-        // 로그인을 안하신 경우 로그인 페이지로 던지기
+        // 기본 처리 내용 정리
     }
 }
---
+++ app/Views/include/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>관리자</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"><!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="assets/plugin/fontawesome-free/css/all.min.css"><!-- Font Awesome -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"><!-- Ionicons -->
+    <link rel="stylesheet" href="assets/plugin/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css"><!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="assets/plugin/icheck-bootstrap/icheck-bootstrap.min.css"><!-- iCheck -->
+    <link rel="stylesheet" href="assets/plugin/jqvmap/jqvmap.min.css"><!-- JQVMap -->
+    <link rel="stylesheet" href="assets/plugin/adminlte3/css/adminlte.css"><!-- Theme style -->
+    <link rel="stylesheet" href="assets/plugin/overlayScrollbars/css/OverlayScrollbars.min.css"><!-- overlayScrollbars -->
+    <link rel="stylesheet" href="assets/plugin/daterangepicker/daterangepicker.css"><!-- Daterange picker -->
+    <link rel="stylesheet" href="assets/plugin/summernote/summernote-bs4.min.css"><!-- summernote -->
+</head>
