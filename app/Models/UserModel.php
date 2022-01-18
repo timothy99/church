d7406025f3d32c78eadfc2bc020bb722cdd60571
timothy99 <joydisk@gmail.com>
2022-01-18 13:59:17 +0900

    회원가입 기능 추가 #16

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,6 +8,8 @@ class UserModel extends Model
     // 사용자 정보 입력
     public function insertUserInfo($data)
     {
+        helper("log_helper");
+
         $user_name = $data["user_name"];
         $user_id = $data["user_id"];
         $user_password = $data["user_password"];
@@ -26,7 +28,13 @@ class UserModel extends Model
             $builder->set("user_id", $user_id);
             $builder->set("user_name", $user_name);
             $builder->set("user_password", $user_password);
-            $db_result = $builder->insert();
+            $builder->set("use_yn", "Y");
+            $builder->set("del_yn", "N");
+            $builder->set("ins_id", $user_id);
+            $builder->set("ins_date", "now()", false);
+            $builder->set("upd_id", $user_id);
+            $builder->set("upd_date", "now()", false);
+            $db_result = $builder->insert()->resultID;
 
             $db->transComplete();
 
