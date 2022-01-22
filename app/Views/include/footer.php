eb005de7ba59d2e7386ddfe3d1eb6d51b99ae638
timothy99 <joydisk@gmail.com>
2022-01-22 23:26:16 +0900

    상대경로 참조 버그 수정 #10

--- app/Views/include/footer.php
+++ app/Views/include/footer.php
@@ -11,21 +11,21 @@
 </div><!-- ./wrapper -->
 
 
-<script src="../assets/plugin/jquery/jquery.min.js"></script><!-- jQuery -->
-<script src="../assets/plugin/jquery-ui/jquery-ui.min.js"></script><!-- jQuery UI 1.11.4 -->
+<script src="/assets/plugin/jquery/jquery.min.js"></script><!-- jQuery -->
+<script src="/assets/plugin/jquery-ui/jquery-ui.min.js"></script><!-- jQuery UI 1.11.4 -->
 <script>$.widget.bridge('uibutton', $.ui.button)</script><!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 
-<script src="../assets/plugin/bootstrap/js/bootstrap.bundle.min.js"></script><!-- Bootstrap 4 -->
-<script src="../assets/plugin/chart.js/Chart.min.js"></script><!-- ChartJS -->
-<script src="../assets/plugin/sparklines/sparkline.js"></script><!-- Sparkline -->
-<script src="../assets/plugin/jqvmap/jquery.vmap.min.js"></script><!-- JQVMap -->
-<script src="../assets/plugin/jqvmap/maps/jquery.vmap.usa.js"></script><!-- JQVMap usa-->
-<script src="../assets/plugin/jquery-knob/jquery.knob.min.js"></script><!-- jQuery Knob Chart -->
-<script src="../assets/plugin/moment/moment.min.js"></script><!-- moment -->
-<script src="../assets/plugin/daterangepicker/daterangepicker.js"></script><!-- daterangepicker -->
-<script src="../assets/plugin/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script><!-- Tempusdominus Bootstrap 4 -->
-<script src="../assets/plugin/summernote/summernote-bs4.min.js"></script><!-- Summernote -->
-<script src="../assets/plugin/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script><!-- overlayScrollbars -->
-<script src="../assets/plugin/adminlte3/js/adminlte.js"></script><!-- AdminLTE App -->
+<script src="/assets/plugin/bootstrap/js/bootstrap.bundle.min.js"></script><!-- Bootstrap 4 -->
+<script src="/assets/plugin/chart.js/Chart.min.js"></script><!-- ChartJS -->
+<script src="/assets/plugin/sparklines/sparkline.js"></script><!-- Sparkline -->
+<script src="/assets/plugin/jqvmap/jquery.vmap.min.js"></script><!-- JQVMap -->
+<script src="/assets/plugin/jqvmap/maps/jquery.vmap.usa.js"></script><!-- JQVMap usa-->
+<script src="/assets/plugin/jquery-knob/jquery.knob.min.js"></script><!-- jQuery Knob Chart -->
+<script src="/assets/plugin/moment/moment.min.js"></script><!-- moment -->
+<script src="/assets/plugin/daterangepicker/daterangepicker.js"></script><!-- daterangepicker -->
+<script src="/assets/plugin/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script><!-- Tempusdominus Bootstrap 4 -->
+<script src="/assets/plugin/summernote/summernote-bs4.min.js"></script><!-- Summernote -->
+<script src="/assets/plugin/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script><!-- overlayScrollbars -->
+<script src="/assets/plugin/adminlte3/js/adminlte.js"></script><!-- AdminLTE App -->
 </body>
 </html>
